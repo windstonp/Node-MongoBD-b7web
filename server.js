00c161b9b21fb68e24b440880cc76f327a6a0428
Alvez <joaor4e@gmail.com>
2020-01-26 22:14:53 -0300

    adicionado coneção com bd

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 const app = require('./app');
+const mongoose = require('mongoose');
 
 require('dotenv').config({ path: 'variables.env' });
+
+//coneção ao DB
+mongoose.connect(process.env.DATABASE, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+mongoose.Promise = global.Promise;
+mongoose.connection.on('error', error => {
+  console.error('Erro: ' + error.message);
+});
+
 app.set('port', process.env.PORT || 7777);
 const server = app.listen(app.get('port'), () => {
   console.log('servidor rodando na porta: ' + server.address().port);
