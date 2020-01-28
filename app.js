bdffdbff94ff91e9d3eafac5dcf765158262b25c
Alvez <joaor4e@gmail.com>
2020-01-28 19:12:10 -0300

    terminado de configurar o template engine

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const mustache = require('mustache-express');
 const router = require('./routes/index');
 //configurações basicas
 const app = express();
 app.use(express.json());
 app.use('/', router);
 app.use('/admin', router);
+app.engine('mst', mustache());
+app.set('view engine', 'mst');
+app.set('views', __dirname + '/views');
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -2,19 +2,9 @@ const express = require('express');
 //rota
 const router = express.Router();
 router.get('/', (req, res) => {
-  res.json(req.query);
-
-  //GET: req.query
-  //POST: req.body
-  //parametros da url: req.params
-  //SEND: envia texto
-  //json: envia json
-});
-router.get('/posts/:slug', (req, res) => {
-  let slug = req.params.slug;
-  res.send('slug do post ' + slug);
-});
-router.get('/admin', (req, res) => {
-  res.send('admin');
+  res.render('home', {
+    nome: 'bonieky',
+    idade: 90
+  });
 });
 module.exports = router;
