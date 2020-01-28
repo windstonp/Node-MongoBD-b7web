ec4d0f63cf18603cec58490d9ce3c5a28116ccd7
Alvez <joaor4e@gmail.com>
2020-01-27 23:53:23 -0300

    terminada a aula 2 explicando um pouco mais sobre as rotas e seus parametros

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = require('./routes/index');
 //configurações basicas
 const app = express();
+app.use(express.json());
 app.use('/', router);
 app.use('/admin', router);
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,17 @@ const express = require('express');
 //rota
 const router = express.Router();
 router.get('/', (req, res) => {
-  res.send('olá Mundo! 2');
+  res.json(req.query);
+
+  //GET: req.query
+  //POST: req.body
+  //parametros da url: req.params
+  //SEND: envia texto
+  //json: envia json
+});
+router.get('/posts/:slug', (req, res) => {
+  let slug = req.params.slug;
+  res.send('slug do post ' + slug);
 });
 router.get('/admin', (req, res) => {
   res.send('admin');
